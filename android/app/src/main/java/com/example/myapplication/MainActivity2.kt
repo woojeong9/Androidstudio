41bc33bbe52654335f0e1785a77d8f28b445ea6f
WoojeongKwon <dnwjd5736@gmail.com>
2020-12-22 00:14:45 +0900

    12월 21일 커밋

--- android/app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ android/app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -5,15 +5,63 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main2.*
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
-        button0.setOnClickListener {
-            Toast.makeText(this, "버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show()
+        var new="0"
+        var old="0"
+        btn1.setOnClickListener {
+            new +="1"
+            result.text=new
         }
-
+        btn2.setOnClickListener {
+            new +="2"
+            result.text=new
+        }
+        btn3.setOnClickListener {
+            new += "3"
+            result.text = new
+        }
+        btn4.setOnClickListener {
+            new += "4"
+            result.text = new
+        }
+        btn5.setOnClickListener {
+            new += "5"
+            result.text = new
+        }
+        btn6.setOnClickListener {
+            new += "6"
+            result.text = new
+        }
+        btn7.setOnClickListener {
+            new += "7"
+            result.text = new
+        }
+        btn8.setOnClickListener {
+            new += "8"
+            result.text = new
+        }
+        btn9.setOnClickListener {
+            new += "9"
+            result.text = new
+        }
+        btn0.setOnClickListener {
+            new += "0"
+            result.text = new
+        }
+        btnP.setOnClickListener {
+            old=(old.toInt()+new.toInt()).toString()
+            new="0"
+            result.text=old
+        }
+        btnE.setOnClickListener{
+            new="0"
+            old="0"
+            result.text="0"
+        }
     }
 }
