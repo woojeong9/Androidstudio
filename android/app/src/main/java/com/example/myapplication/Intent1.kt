7f3ca0d53af48f998ce892e84ded5c77efb38e3f
WoojeongKwon <dnwjd5736@gmail.com>
2020-12-24 23:36:08 +0900

    12월 24일 커밋

--- android/app/src/main/java/com/example/myapplication/Intent1.kt
+++ android/app/src/main/java/com/example/myapplication/Intent1.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_intent1.*
 
 class Intent1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent1)
+
+        var acb=10
+        var b=acb*10
+        var c=acb*100
+            change_activity.setOnClickListener{
+            val intent=Intent(this@Intent1,Intent2::class.java)
+            intent.putExtra("number1",1)
+            intent.putExtra("number2",2)
+            startActivity(intent)
+        }
     }
 }
