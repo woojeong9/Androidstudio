ca73e7deac8b9fe094b29baee385eb3e9cdd66b5
woojeong9 <dnwjd5736@gmail.com>
2021-01-04 22:02:31 +0900

    1월 4일 커밋

---
+++ android/app/src/main/java/com/example/myapplication/Account.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
